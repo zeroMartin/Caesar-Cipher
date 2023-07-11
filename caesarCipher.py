e7330a6c74576b3b4f26c23c1b85387187bce8f5
zeroMartin <brckovic.martin95@gmail.com>
2023-07-11 19:11:26 +0200

    parsing for encryption done

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,15 +1,25 @@
+import argparse
+
 text_to_encrypt = 'martin'
 text_to_decrypt = 'pduwlq'
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--encrypt')
+parser.add_argument('-s', '--shift', type=int)
+args = parser.parse_args()
+
+
+# Function to encrypt text
 def caesar_encryption():
-    s = 3
+    
     encrypted_text = ''
 
-    for character in text_to_encrypt:
-        encrypted_character = chr(((ord(character) + s - 97) % 26) + 97)
+    for character in args.encrypt:
+        encrypted_character = chr(((ord(character) + args.shift - 97) % 26) + 97)
         encrypted_text += encrypted_character
     return encrypted_text
 
+# Function to decrypt text
 def caesar_decryption():
     s = 3
     decrypted_text = ''
