7848aa5de7f3f5a6e00e90d6e52163425008b96c
zeroMartin <brckovic.martin95@gmail.com>
2023-07-14 20:30:20 +0200

    code recognizes upper and lower case letters

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,7 +1,7 @@
 import argparse
 
 text_to_encrypt = 'martin'
-text_to_decrypt = 'pduwlq'
+text_to_decrypt = 'churPduwlq'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--encrypt')
@@ -15,8 +15,12 @@ def caesar_encryption():
     encrypted_text = ''
 
     for character in args.encrypt:
-        encrypted_character = chr(((ord(character) + args.shift - 97) % 26) + 97)
-        encrypted_text += encrypted_character
+        if character.isupper():
+            encrypted_character = chr(((ord(character) + args.shift - 65) % 26) + 65)
+            encrypted_text += encrypted_character
+        elif character.islower():
+            encrypted_character = chr(((ord(character) + args.shift - 97) % 26) + 97)
+            encrypted_text += encrypted_character
     return encrypted_text
 
 # Function to decrypt text
@@ -25,8 +29,12 @@ def caesar_decryption():
     decrypted_text = ''
 
     for character in text_to_decrypt:
-        decrypted_character = chr(((ord(character) - s - 97) % 26) + 97)
-        decrypted_text += decrypted_character
+        if character.isupper():
+            decrypted_character = chr(((ord(character) - s - 65) % 26) + 65)
+            decrypted_text += decrypted_character
+        if character.islower():
+            decrypted_character = chr(((ord(character) - s - 97) % 26) + 97)
+            decrypted_text += decrypted_character
     return decrypted_text
 
 encrypted_text = caesar_encryption()
